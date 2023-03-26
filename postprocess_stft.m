d46d8992e8b7f8b8a7d9e04ee891a5368051dcc3
elvisfox <elvisfox@github.com>
2023-03-26 20:19:29 +0300

    Switch to complex input and output signals

--- postprocess_stft.m
+++ postprocess_stft.m
@@ -11,20 +11,21 @@ in_data = in_data(1 : out_data.size);
 err_vect = out_data - in_data;
 err_lsb = err_vect * 2^15;
 
-max_err_lsb = round(max(abs(err_lsb)));
-rms_err_lsb = sqrt(sum((err_lsb .^ 2) ./ length(err_lsb)));
-err_text = sprintf("Max error: %d [LSB_{15}]\nRMS error: %.3f [LSB_{15}]", max_err_lsb, rms_err_lsb);
+max_err_lsb = max(abs([err_lsb.real ; err_lsb.imag]));
+rms_err_lsb = sqrt(sum((abs(err_lsb) .^ 2) ./ length(err_lsb_real)));
+err_text = sprintf("Max error: %.1f [LSB_{15}]\nRMS error: %.3f [LSB_{15}]", max_err_lsb, rms_err_lsb);
 fprintf("%s\n", err_text);
 
 %% Plot results
 figure;
 ax1 = subplot(2,1,1);
-plot([in_data out_data]);
-legend(["input" "output"]);
+plot([in_data.real in_data.imag out_data.real out_data.imag]);
+legend(["input.re" "input.im" "output.re" "output.im"]);
 ylabel("Data");
 
 ax2 = subplot(2,1,2);
-plot(err_lsb);
+plot([err_lsb.real err_lsb.imag]);
+legend(["re" "im"])
 linkaxes([ax1 ax2], 'x');
 ylabel("Error [LSB_{15}]");
 xlabel("Sample");
